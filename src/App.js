b79d282cc69dcaa0d466b0011efce29054cc0350
André Lashley <andre.lashley@gmail.com>
2022-10-24 13:50:50 -0700

    Add mock translations data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const translations = [
+    {
+      id: 1,
+      key: "2021HLTH0075-002303",
+      url: "https://bcgovnews.azureedge.net/translations/releases/2021HLTH0075-002303/12.1.21_COVID_Chinese(traditional).pdf",
+    },
+    {
+      id: 2,
+      key: "2021HLTH0075-002303",
+      url: "https://bcgovnews.azureedge.net/translations/releases/2021HLTH0075-002303/12.1.21_COVID_French.pdf",
+    },
+    {
+      id: 3,
+      key: "2021HLTH0075-002303",
+      url: "https://bcgovnews.azureedge.net/translations/releases/2021HLTH0075-002303/12.1.21_COVID_Punjabi.pdf",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>GCPE Business Insights</h1>
+      {translations.length === 0 ?
+        <p>There are no translations.</p>
+      :
+      translations.map((translation) => {
+        return <p key={translation.id}>{translation.key}:  {translation.url}</p>;
+      })}
+    </>
   );
 }
 
