ec43158d5e3d60f66f4fdcad3c31372b333645bd
André Lashley <andre.lashley@gmail.com>
2022-12-21 13:38:49 -0800

    Misc. fixes

--- src/components/Translations.js
+++ src/components/Translations.js
@@ -34,19 +34,59 @@ export default function Translations() {
 
   return (
     <>
-      <div style={{ marginBottom: 50 }}>
+      <div>
         <h1>Monthly Overview</h1>
         <p>
-          {monthlyNewsReleaseVolume} releases translated in {monthName}
+          <strong>Published to BC Gov News</strong>
+        </p>
+        <p>
+          <strong>{monthlyNewsReleaseVolume}</strong> releases translated in{" "}
+          {monthName}
           <br />
-          {translationsVolumeByMonth} documents translated in {monthName}
+          <strong>{translationsVolumeByMonth}</strong> documents translated in{" "}
+          {monthName}
         </p>
       </div>
       {releasesTranslatedByMinistry === undefined ? (
         <Spinner animation="border" />
       ) : (
         <>
-          <h2>News Releases By Ministry</h2>
+          {languageCounts === null ? (
+            <p>Could not retrieve languages.</p>
+          ) : (
+            <>
+              <h2 style={{ marginTop: 50 }}>Translations by Language</h2>
+              {languageCounts.length === 0 ? (
+                <p>There are no language counts.</p>
+              ) : (
+                <Table striped bordered>
+                  <thead>
+                    <tr>
+                      <th>Language</th>
+                      <th className="centerTd">Count</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {languageCounts.map((item, index) => {
+                      return (
+                        <tr key={item.language}>
+                          <td>{item.language}</td>
+                          <td className="centerTd">{item.count}</td>
+                        </tr>
+                      );
+                    })}
+                    <tr>
+                      <td></td>
+                      <td className="centerTd">
+                        <strong>Total {translationsVolumeByMonth}</strong>
+                      </td>
+                    </tr>
+                  </tbody>
+                </Table>
+              )}
+            </>
+          )}
+          <h2 style={{ marginTop: 50 }}>News Releases By Ministry</h2>
           {releasesTranslatedByMinistry === null ? (
             <p>Could not retrieve news releases by ministry.</p>
           ) : (
@@ -58,7 +98,7 @@ export default function Translations() {
                   <thead>
                     <tr>
                       <th>Ministry</th>
-                      <th>Count</th>
+                      <th className="centerTd">Count</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -66,36 +106,7 @@ export default function Translations() {
                       return (
                         <tr key={item.ministry}>
                           <td>{item.ministry}</td>
-                          <td>{item.count}</td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </Table>
-              )}
-            </>
-          )}
-          {languageCounts === null ? (
-            <p>Could not retrieve languages.</p>
-          ) : (
-            <>
-              <h2 style={{ marginTop: 50 }}>Totals by Language</h2>
-              {languageCounts.length === 0 ? (
-                <p>There are no language counts.</p>
-              ) : (
-                <Table striped bordered>
-                  <thead>
-                    <tr>
-                      <th>Language</th>
-                      <th>Count</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {languageCounts.map((item, index) => {
-                      return (
-                        <tr key={item.language}>
-                          <td>{item.language}</td>
-                          <td>{item.count}</td>
+                          <td className="centerTd">{item.count}</td>
                         </tr>
                       );
                     })}
@@ -116,7 +127,7 @@ export default function Translations() {
                   <thead>
                     <tr>
                       <th>News Release Key</th>
-                      <th>Azure Urls</th>
+                      <th>URL</th>
                     </tr>
                   </thead>
                   <tbody>
