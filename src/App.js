08c9e2089ca79b3727b2b764b99316e1db518289
Alessia Yan Chen <ayanchen@Alessias-MacBook-Pro.local>
2023-01-13 15:30:20 -0800

    navbar style

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import Container from 'react-bootstrap/Container';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 import ApiProvider from "./contexts/ApiProvider";
-import Header from './components/Header';
-import ExplorePage from './pages/ExplorePage';
-import UserPage from "./pages/UserPage";
-import LoginPage from './pages/LoginPage';
-import TranslationsPage from './pages/TranslationsPage';
-import RollupPage from "./pages/RollupPage";
-import RegistrationPage from "./pages/RegistrationPage";
-import HistoryPage from "./pages/HistoryPage";
-import HistoryListPage from "./pages/HistoryListPage";
+import Container from "react-bootstrap/Container";
+import ExplorePage from "./pages/ExplorePage";
 import FlashProvider from "./contexts/FlashProvider";
-import UserProvider from './contexts/UserProvider';
+import Header from "./components/Header";
+import HistoryPage from "./pages/HistoryPage";
+import LoginPage from "./pages/LoginPage";
 import PrivateRoute from "./components/PrivateRoute";
 import PublicRoute from "./components/PublicRoute";
+import RegistrationPage from "./pages/RegistrationPage";
+import RollupPage from "./pages/RollupPage";
+import TranslationsPage from "./pages/TranslationsPage";
+import UserPage from "./pages/UserPage";
+import UserProvider from "./contexts/UserProvider";
 
 export default function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,40 @@
-import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
-import { useIsAuthenticated } from "@azure/msal-react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import { SignOutButton } from "./SignOutButton";
 import logo from "../logo.png";
-
+import { useIsAuthenticated } from "@azure/msal-react";
 
 export default function Header() {
   const isAuthenticated = useIsAuthenticated();
-
+  const titleDiv = {
+    borderLeft: "1px solid #fcba19",
+    paddingLeft: "20px",
+    fontSize: "1.5em",
+    fontWeight: "strong",
+  };
   return (
-    <Navbar variant="dark" sticky="top" className="Header nav-bg">
+    <Navbar
+      variant="light"
+      sticky="top"
+      className="Header nav-bg"
+      expand="none"
+    >
       <Container>
         <Navbar.Brand>
           <img src={logo} className="logo" alt="logo" />
-          GCPE Business Insights
+          <span style={{ ...titleDiv }}>GCPE Business Insights</span>
         </Navbar.Brand>
         {isAuthenticated ? <SignOutButton /> : <></>}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link1</Nav.Link>
+            <Nav.Link href="#link">Link2</Nav.Link>
+            <Nav.Link href="#link">Link3</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
